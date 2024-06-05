8f7b48f0428d07053e0b9ab5ed11a8fbcf2dd059
aidilbaihaqi <aidillmusirjun@gmail.com>
2024-06-05 19:47:32 +0700

    edit: web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\ArsipController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\NotulensiController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SiaranController;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
 
@@ -11,6 +14,33 @@ Route::get('/', function () {
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    Route::controller(SiaranController::class)->group(function() {
+        Route::get('/siaran', 'index')->name('siaran.index');
+        Route::get('/siaran/tambah', 'create')->name('siaran.create');
+        Route::post('/siaran/tambah', 'store')->name('siaran.store');
+        Route::get('/siaran/ubah/{kode_siaran}', 'edit')->name('siaran.edit');
+        Route::post('/siaran/ubah/{kode_siaran}', 'update')->name('siaran.update');
+        Route::get('/siaran/hapus/{kode_siaran}', 'destroy')->name('siaran.destroy');
+    });
+
+    Route::controller(ArsipController::class)->group(function() {
+        Route::get('/arsip', 'index')->name('arsip.index');
+        Route::get('/arsip/tambah', 'create')->name('arsip.create');
+        Route::post('/arsip/tambah', 'store')->name('arsip.store');
+        Route::get('/arsip/ubah/{id}', 'edit')->name('arsip.edit');
+        Route::post('/arsip/ubah/{id}', 'update')->name('arsip.update');
+        Route::get('/arsip/hapus/{id}', 'destroy')->name('arsip.destroy');
+    });
+
+    Route::controller(NotulensiController::class)->group(function() {
+        Route::get('/notulensi', 'index')->name('notulensi.index');
+        Route::get('/notulensi/tambah', 'create')->name('notulensi.create');
+        Route::post('/notulensi/tambah', 'store')->name('notulensi.store');
+        Route::get('/notulensi/ubah/{id}', 'edit')->name('notulensi.edit');
+        Route::post('/notulensi/ubah/{id}', 'update')->name('notulensi.update');
+        Route::get('/notulensi/hapus/{id}', 'destroy')->name('notulensi.destroy');
+    });
 });
 
 Route::middleware('auth')->group(function () {
