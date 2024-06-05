b866f5dce2733cbb5a405fb9a862f96684a7a54d
aidilbaihaqi <aidillmusirjun@gmail.com>
2024-06-05 19:42:19 +0700

    crud controller: siaran, notulensi, arsip

--- app/Http/Controllers/NotulensiController.php
+++ app/Http/Controllers/NotulensiController.php
@@ -3,8 +3,67 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Notulensi;
+use Illuminate\Support\Facades\Validator;
 
 class NotulensiController extends Controller
 {
-    //
+    public function index() {
+        $data = Notulensi::all();
+        return view('notulensi.index', [
+            'title' => 'Data Notulensi',
+            'data' => $data
+        ]);
+    }
+    public function create() {
+        return view('notulensi.create', [
+            'title' => 'Tambah Notulensi'
+        ]);
+    }
+    public function store(Request $request) {
+        $request = Validator::make($request->all(), [
+            'kode_siaran' => 'required',
+            'notulen' => 'required'
+        ]);
+
+        if($request->fails()) {
+            return 'alert("Gagal validasi")';
+        }
+
+        Notulensi::create($request->all());
+
+        return redirect()->route('notulensi.index')
+                        ->with('success', 'Notulensi berhasil ditambahkan!');
+    }
+    public function edit($id) {
+        $data = Notulensi::findOrFail($id);
+        return view('notulensi.edit', [
+            'title' => 'Ubah Notulensi',
+            'data' => $data
+        ]);
+    }
+    public function update(Request $request, $id) {
+        $request = Validator::make($request->all(), [
+            'kode_siaran' => 'required',
+            'notulen' => 'required'
+        ]);
+
+        if($request->fails()) {
+            return 'alert("Gagal validasi")';
+        }
+
+        $data = Notulensi::find($id);
+        $data->update($request->all());
+
+        return redirect()->route('notulensi.index')
+                        ->with('success', 'Notulensi berhasil diubah!');
+    }
+    public function destroy($id) {
+        $data = Notulensi::find($id);
+        $data->delete();
+    
+        return redirect()->route('notulensi.index')
+                        ->with('success', 'Notulensi berhasil dihapus!');
+    }
+
 }
--- app/Http/Controllers/SiaranController.php
+++ app/Http/Controllers/SiaranController.php
@@ -3,8 +3,73 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Siaran;
 
 class SiaranController extends Controller
 {
-    //
+    public function index() {
+        $data = Siaran::all();
+        return view('siaran.index', [
+            'title' => 'Data Siaran',
+            'data' => $data
+        ]);
+    }
+    public function create() {
+        return view('siaran.create', [
+            'title' => 'Tambah Siaran'
+        ]);
+    }
+    public function store(Request $request) {
+        $request = Validator::make($request->all(), [
+            'kode_siaran' => 'required|unique:siarans',
+            'judul' => 'required|max:150',
+            'tgl_waktu' => 'required',
+            'nama_penyiar' => 'required',
+            'status' => 'required'
+        ]);
+
+        if($request->fails()) {
+            return 'alert("Gagal validasi")';
+        }
+
+        Siaran::create($request->all());
+
+        return redirect()->route('siaran.index')
+                        ->with('success', 'Siaran berhasil dijadwalkan!');
+    }
+    public function edit($kode_siaran) {
+        $data = Siaran::where('kode_siaran', $kode_siaran)->first();
+        return view('siaran.edit', [
+            'title' => 'Ubah Siaran',
+            'data' => $data
+        ]);
+    }
+    public function update(Request $request, $kode_siaran) {
+        $request = Validator::make($request->all(), [
+            'kode_siaran' => 'required',
+            'judul' => 'required|max:150',
+            'tgl_waktu' => 'required',
+            'nama_penyiar' => 'required',
+            'status' => 'required'
+        ]);
+
+        if($request->fails()) {
+            return 'alert("Gagal validasi")';
+        }
+
+        $data = Siaran::find($kode_siaran);
+        $data->update($request->all());
+
+        return redirect()->route('siaran.index')
+                        ->with('success', 'Siaran berhasil diubah!');
+    }
+    public function destroy($kode_siaran) {
+        $data = Siaran::find($kode_siaran);
+        $data->delete();
+    
+        return redirect()->route('siaran.index')
+                        ->with('success', 'Siaran berhasil dihapus!');
+    }
+
 }
